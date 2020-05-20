0a3ff1f547a87c3cab85928ce1c8f60d164ca848
Pedro F Linhares <pedroflinhares@edu.unifor.br>
2020-05-19 23:17:55 -0300

    :whale: Migração para o gradle, deploy com shadowJar

--- src/main/java/projeto_1/user/beans/Token.java
+++ src/main/java/projeto_1/user/beans/Token.java
@@ -3,9 +3,21 @@ package projeto_1.user.beans;
 import java.io.Serializable;
 
 public class Token implements Serializable {
-    public final String token;
+    public String token;
+
+    public Token() {
+        this.token = "";
+    }
 
     public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
         this.token = token;
     }
 }
